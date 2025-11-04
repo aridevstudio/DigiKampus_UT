eb6a18ae33d45cb4da2412150265a2b602e898e8
AjiRamdaniARM <hilmankeren0936@gmail.com>
2025-11-04 16:19:42 +0700

    aju push

---
+++ database/migrations/2025_10_11_163642_create_courses.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+                $table->id('id_course');
+                $table->string('kode_course')->unique();
+                $table->string('nama_course');
+                $table->text('deskripsi')->nullable();
+                $table->unsignedBigInteger('id_dosen')->nullable();
+                $table->unsignedBigInteger('id_jurusan')->nullable();
+                $table->text('thumbnail')->nullable();
+                $table->enum('status', ['draft', 'aktif', 'selesai'])->default('draft');
+                $table->timestamps();
+
+                // Relasi ke tabel users (dosen)
+                $table->foreign('id_dosen')->references('id_user')->on('users')->onDelete('set null');
+
+                // Relasi ke tabel jurusans
+                $table->foreign('id_jurusan')->references('id_jurusan')->on('jurusans')->onDelete('set null');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
