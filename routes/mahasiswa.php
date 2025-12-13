9742ef118dd4d4e7f83411a50fdd86398c1293af
RenzIP <zenkun.enterkill13@gmail.com>
2025-12-13 22:42:25 +0700

    feat: Add Mahasiswa authentication, password recovery, and protected dashboard routes.

--- routes/mahasiswa.php
+++ routes/mahasiswa.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('mahasiswa')
     ->middleware(RedirectIfAuthenticatedMahasiswa::class)
     ->group(function () {
-        Route::get('/login/mahasiswa', [MahasiswaController::class, 'showLoginForm'])->name('mahasiswa.login');
-        Route::post('/login/mahasiswa', [MahasiswaController::class, 'showLoginFormPost'])->name('mahasiswa.post');
+        Route::get('/login', [MahasiswaController::class, 'showLoginForm'])->name('mahasiswa.login');
+        Route::post('/login', [MahasiswaController::class, 'showLoginFormPost'])->name('mahasiswa.post');
         Route::get('/forgot-password', [MahasiswaController::class, 'showForgotPasswordForm'])->name('mahasiswa.forgot-password');
         Route::post('/forgot-password', [MahasiswaController::class, 'sendForgotPasswordOtp'])->name('mahasiswa.forgot-password.post');
         Route::get('/verify-otp', [MahasiswaController::class, 'showVerifyOtpForm'])->name('mahasiswa.verify-otp');
@@ -24,9 +24,9 @@ Route::prefix('mahasiswa')
     ->middleware(EnsureAuthenticatedMahasiswa::class)
     ->group(function () {
         // get
-        Route::get('/mahasiswa/dashboard', [MahasiswaController::class, 'showDashboard'])->name('mahasiswa.dashboard');
-        Route::get('/mahasiswa/calendar', [MahasiswaController::class, 'showCalendar'])->name('mahasiswa.calendar');
+        Route::get('/dashboard', [MahasiswaController::class, 'showDashboard'])->name('mahasiswa.dashboard');
+        Route::get('/calendar', [MahasiswaController::class, 'showCalendar'])->name('mahasiswa.calendar');
 
         // post
-        Route::post('/mahasiswa/logout', [MahasiswaController::class, 'logout'])->name('mahasiswa.logout');
+        Route::post('/logout', [MahasiswaController::class, 'logout'])->name('mahasiswa.logout');
     });
