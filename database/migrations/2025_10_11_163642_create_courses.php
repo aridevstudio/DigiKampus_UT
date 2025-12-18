d1c3eb5a8f828d2f42150ae53a5c54a0707f56e5
AjiRamdaniARM <hilmankeren0936@gmail.com>
2025-12-18 23:17:01 +0700

    push vercel

--- database/migrations/2025_10_11_163642_create_courses.php
+++ database/migrations/2025_10_11_163642_create_courses.php
@@ -34,6 +34,9 @@ return new class extends Migration {
      */
     public function down(): void
     {
+        Schema::table('assignments', function (Blueprint $table) {
+        $table->dropForeign(['id_course']); // nama kolom FK di assignments
+        });
         Schema::dropIfExists('courses');
     }
 };
