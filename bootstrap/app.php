ed06aab112940cc39de26b53f434c89cd3b69056
RenzIP <zenkun.enterkill13@gmail.com>
2026-02-23 18:24:16 +0700

    oioioi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Enable Sanctum SPA/session auth on API routes.
+        $middleware->statefulApi();
+
         $middleware->alias([
             'auth.custom' => \App\Http\Middleware\Authenticate::class,
         ]);
